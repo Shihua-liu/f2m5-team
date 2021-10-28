84e67433e69af67e67135392579e2429ab5270a3
Robin Zhao <titanen.1987@hotmail.com>
2021-10-28 09:44:12 +0200

    aanpassingen aan views/layout

--- f2m5-social-responsibility-main/_private/views/layouts/website.php
+++ f2m5-social-responsibility-main/_private/views/layouts/website.php
@@ -39,4 +39,14 @@
 <script>
     AOS.init();
 </script>
+<script type="text/javascript">
+    var counter = 1;
+    setInterval(function(){
+      document.getElementById('radio' + counter).checked = true;
+      counter++;
+      if(counter > 3){
+        counter = 1;
+      }
+    }, 4000);
+    </script>
 </html>
--- f2m5-social-responsibility-main/_private/views/layouts/word-transformer.php
+++ f2m5-social-responsibility-main/_private/views/layouts/word-transformer.php
@@ -11,6 +11,7 @@
 	<?php endif; ?>
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="../js/word-transformer.js" defer></script>
 </head>
     <!-- test voor brach push -->
 <body>
@@ -32,5 +33,4 @@
 <script>
     AOS.init();
 </script>
-<script src="../js/word-transformer.js"></script>
 </html>
